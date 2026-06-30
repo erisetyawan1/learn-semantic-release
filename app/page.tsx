b6eed58633b5510e1f2189494fd6f93984c6453f
erisetyawan1 <muhammad.eri@skyworx.co.id>
2026-06-30 10:13:39 +0700

    feat!: this is new feature breaking changes!

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,9 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             this is version new feature fix
           </h1>
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            this is another new version!
+          </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
